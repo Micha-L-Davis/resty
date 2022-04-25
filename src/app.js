07af2689747a007765ebcbacfc5759641a6fa80f
Micha Davis <m.leigh.davis@gmail.com>
2022-04-25 15:22:20 -0700

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './app.scss';
 
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
@@ -24,11 +22,11 @@ class App extends React.Component {
     const data = {
       count: 2,
       results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
       ],
     };
-    this.setState({data, requestParams});
+    this.setState({ data, requestParams });
   }
 
   render() {
