913d8d79c3c4549080932a357a0505105214393a
Micha Davis <m.leigh.davis@gmail.com>
2022-04-25 15:22:48 -0700

    refactor header to function

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
-}
+let Header = () => (
+  <header>
+    <h1>RESTy</h1>
+  </header>
+);
+
 
 export default Header;
