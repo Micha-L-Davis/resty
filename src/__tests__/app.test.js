7213e79191f85220fc779fde9dc544a71d31acfd
Micha Davis <m.leigh.davis@gmail.com>
2022-04-27 20:50:12 -0700

    Add mock api test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,17 +1,33 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../app.js'
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import App from '../app.js';
+
+const server = setupServer(
+  rest.get('*', (req, res, ctx) => {
+    return res(ctx.json({ data: [{ name: 'testington' }] }));
+  }),
+  rest.post('*', (req, res, ctx) => {
+    return res(ctx.json({ data: [{ name: 'testington' }] }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Form submission tests', () => {
   render(<App />);
 
   let form = screen.getByTestId('form');
   let selectMethod = screen.getByTestId('selectMethod');
+  let resultArea = screen.getByTestId('result-area');
 
   it('should update the rest method, request url, and response area on submit', () => {
     let restMethod = screen.getByTestId('rest-method');
     let requestUrl = screen.getByTestId('request-url');
-    let resultArea = screen.getByTestId('result-area')
+
 
     fireEvent.click(selectMethod, { target: { id: 'post' } });
     fireEvent.submit(form, { target: { url: { value: 'test url' } } });
@@ -20,6 +36,10 @@ describe('Form submission tests', () => {
     expect(restMethod).toContainHTML('post');
     expect(resultArea).not.toBeNull();
   });
+
+  it('should get results from a mock API', async () => {
+    setInterval(() => expect(resultArea).toContainHTML('testington'), 2000);
+  })
 });
 
 
